95ab3ebb1221d2df9e0c9047d1593cb2f47f0642
nbnrain <jcoop@ceh.ac.uk>
2015-11-19 16:33:34 +0000

    updating to thingspeak channels more generic now

--- mongoToThingspeak.py
+++ mongoToThingspeak.py
@@ -7,24 +7,27 @@ import time
 import urllib
 import urllib2
 import unicodedata
-#from pymongo import MongoClient
 from dateutil.parser import *
 
-def senddata(measurement):
+def sendAnUpdate(measurement, parameters, thingspeakKey):
+  
+  #Here is an example illustrating the structure of the dictionary being constructed in the next bit of code
+  # values = {
+  #   'key': THINGSPEAKKEY, 
+  #   'created_at': measurement["timestamp"],
+  #   'field1': measurement["air_humidity_1"],
+  #   'field2': measurement["air_temp_1"],
+  #   'field3': measurement["soil_moisture_1"],
+  # }
   values = {
-    'key': THINGSPEAKKEY, 
+    'key': thingspeakKey, 
     'created_at': measurement["timestamp"],
-    'field1': measurement["air_humidity_1"],
-    'field2': measurement["air_temp_1"],
-    'field3': measurement["air_temp_2"],
-    'field4': measurement["soil_moisture_1"],
-    'field5': measurement["soil_temp_1"],
-    'field6': measurement["soil_temp_2"],
-    'field7': measurement["soil_temp_3"],
-    'field8': measurement["surface_flow"]
   }
+  for index, parameter in enumerate(parameters, start = 1):
+    values['field' + str(index)] = measurement[parameter]
+
   postdata = urllib.urlencode(values)
-  req = urllib2.Request(THINGSPEAKURL + "/update",postdata)
+  req = urllib2.Request(THINGSPEAKURL + "/update", postdata)
   log = time.strftime("%d-%m-%Y,%H:%M:%S") + ","
   try:
     response = urllib2.urlopen(req, None, 5)
@@ -39,8 +42,8 @@ def senddata(measurement):
     log = log + 'Unknown error'
   print log  
 
-def getandsend(latestThingspeakMeasurement):
-  print 'getting data out of mongodb'
+def sendUpdatesSince(latestThingspeakMeasurement, sensor, parameters, thingspeakKey):
+  print 'Getting measurements from mongodb that are post ' + str(latestThingspeakMeasurement)
 
   connectionUri = "mongodb://{0}:{1}@localhost/envdata".format(MONGOUSER, MONGOPASSWORD, MONGOUSERDB)
   client = pymongo.MongoClient(connectionUri)
@@ -50,44 +53,36 @@ def getandsend(latestThingspeakMeasurement):
 
   #Limit to just soil sensors
   query = {
-    "sensor": "soil",
+    "sensor": sensor,
     "timestamp": {
       "$gte": latestThingspeakMeasurement
     }
   }
 
-  projection = {
-    "timestamp": 1, 
-    "air_humidity_1": 1,
-    "air_humidity_2": 1,
-    "air_humidity_3": 1,
-    "air_temp_1": 1,
-    "air_temp_2": 1,
-    "air_temp_3": 1,
-    "soil_moisture_1": 1,
-    "soil_moisture_2": 1,
-    "soil_moisture_3": 1,
-    "soil_temp_1": 1,
-    "soil_temp_2": 1,
-    "soil_temp_3": 1,
-    "surface_flow": 1,
-    "trial": 1,
-    "_id":1
-  }
-
+  #Here is an example of the projection dictionary that the next bit of code creates
+  # projection = {
+  #   "timestamp": 1, 
+  #   "air_humidity_1": 1,
+  #   "air_temp_1": 1,
+  #   "soil_moisture_1": 1,
+  # }
+  projection = {"timestamp": 1}
+  for parameter in parameters:
+    projection[parameter] = 1
+  
   previousTimestamp = ""
   #The batchsize added to the following query ensures we don't hit mongodb's 10 min timeout on the cursor
   for measurement in measurements.find(query,projection).sort([("timestamp", pymongo.ASCENDING)]).batch_size(20):
     #The data contains duplicate records, remove them using the timestamp as a test
     if (measurement["timestamp"] != previousTimestamp):
-      senddata(measurement)
+      sendAnUpdate(measurement, parameters, thingspeakKey)
       #Thingspeak is limited to one update per channel per 15s
       time.sleep(15)
     previousTimestamp = measurement["timestamp"]
 
-def latestThingspeakUpdate(url):
+def latestThingspeakUpdate(url, thingspeakKey):
   defaultStartDate = parse('2000-01-01T00:00:00Z')
-  url = url + "?key=" + THINGSPEAKKEY
+  url = url + "?key=" + thingspeakKey
   try:
     data = urllib2.urlopen(url).read()
     if ("\"-1\"" == data):
@@ -106,7 +101,12 @@ def config():
   global MONGOPASSWORD
   global MONGOUSERDB
   global THINGSPEAKURL
-  global THINGSPEAKKEY
+  global THINGSPEAKKEY_SOIL
+  global THINGSPEAKCHANNEL_SOIL
+  global THINGSPEAKKEY_GPS
+  global THINGSPEAKCHANNEL_GPS
+  global THINGSPEAKKEY_ACC
+  global THINGSPEAKCHANNEL_ACC
   f = open('mongoToThingspeak.cfg','r')
   data = f.read().splitlines()
   f.close()
@@ -114,12 +114,39 @@ def config():
   MONGOPASSWORD = data[1]
   MONGOUSERDB = data[2]
   THINGSPEAKURL = data[3]
-  THINGSPEAKKEY = data[4]
+  THINGSPEAKKEY_SOIL = data[4]
+  THINGSPEAKCHANNEL_SOIL = data[5]
+  THINGSPEAKKEY_GPS = data[6]
+  THINGSPEAKCHANNEL_GPS = data[7]
+  THINGSPEAKKEY_ACC = data[8]
+  THINGSPEAKCHANNEL_ACC = data[9]
 
 def main():
   config()
-  latestThingspeakMeasurement =  latestThingspeakUpdate(THINGSPEAKURL + "/channels/66379/feeds/last")
-  getandsend(latestThingspeakMeasurement)
+  
+  #Identifier in mongodb documents for sensor type - seems that it can be 'soil', 'gps' or 'acc'
+  sensorType = "acc"
+  
+  parameters = None
+  latestThingspeakMeasurement = None
+  thingspeakkey = None
+  thingspeakchannel = None
+
+  if (sensorType == "soil"):
+    parameters = ["air_humidity_1","air_temp_1","air_temp_2","soil_moisture_1","soil_temp_1","soil_temp_2","soil_temp_3","surface_flow"]
+    thingspeakkey = THINGSPEAKKEY_SOIL
+    thingspeakchannel = THINGSPEAKCHANNEL_SOIL
+  elif (sensorType == "gps"):
+    parameters = ["lat","lon","gps_qual","rssi","batt_v"]
+    thingspeakkey = THINGSPEAKKEY_GPS
+    thingspeakchannel = THINGSPEAKCHANNEL_GPS
+  elif (sensorType == "acc"):
+    parameters = ["x","y","z","rssi","batt_v","src_addr"]
+    thingspeakkey = THINGSPEAKKEY_ACC
+    thingspeakchannel = THINGSPEAKCHANNEL_ACC
+
+  latestThingspeakMeasurement =  latestThingspeakUpdate(THINGSPEAKURL + "/channels/" + thingspeakchannel + "/feeds/last", thingspeakkey)
+  sendUpdatesSince(latestThingspeakMeasurement, sensorType, parameters, thingspeakkey)
 
 if __name__=="__main__":
   main()
